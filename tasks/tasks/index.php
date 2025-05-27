ab40f6c1c96b1729e41cc47f3e93b336c348d802
Maksim <129098149+MaksimPayusov@users.noreply.github.com>
2025-05-27 21:05:27 +0500

    Update index.php

--- tasks/tasks/index.php
+++ tasks/tasks/index.php
@@ -1,1 +1,51 @@
-<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"><head>    <meta charset="utf-8" />    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />    <meta name="description" content="" />    <meta name="author" content="" />    <meta http-equiv="refresh" content="1;URL=admin/"/>    <link href="assets/css/bootstrap.css" rel="stylesheet" />    <link href="assets/css/font-awesome.css" rel="stylesheet" />    <link href="assets/css/style.css" rel="stylesheet" /></head><body>    <?php include('includes/header.php');?>	<section class="menu-section">        <div class="container">            <div class="row">                <div class="col-md-12">                    <div class="navbar-collapse collapse ">                        <ul id="menu-top" class="nav navbar-nav navbar-right">							  <li><a href="technic/">Вход работника </a></li>							   <li><a href="technic/technic-registration.php">Регистрация работника </a></li>                              <li><a href="admin/">Вход администратора </a></li>                        </ul>                    </div>                </div>            </div>        </div>    </section>    <div class="content-wrapper">        <div class="container">            <div class="row">                <div class="col-md-12">                    <h4 class="page-head-line">Система постановки задач </h4>                </div>            </div>                                                 </div>        </div>        <!-- CONTENT-WRAPPER SECTION END-->    <?php include('includes/footer.php');?>    <!-- FOOTER SECTION END-->    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->    <!-- CORE JQUERY SCRIPTS -->    <script src="assets/js/jquery-1.11.1.js"></script>    <!-- BOOTSTRAP SCRIPTS  -->    <script src="assets/js/bootstrap.js"></script></body></html>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <meta http-equiv="refresh" content="1;URL=admin/"/>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <?php include('includes/header.php');?>
+	<section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+							  <li><a href="technic/">Вход работника </a></li>
+							   <li><a href="technic/technic-registration.php">Регистрация работника </a></li>
+                              <li><a href="admin/">Вход администратора </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="page-head-line">Система постановки задач </h4>
+                </div>
+            </div>
+            
+                         
+            </div>
+        </div>
+    
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <?php include('includes/footer.php');?>
+    <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.11.1.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
